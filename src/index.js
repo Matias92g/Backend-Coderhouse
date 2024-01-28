f20e32f6ae0b582d6f55838f5f2cf670cea8df02
Matias Gonzalez <matias92meg@gmail.com>
2024-01-28 12:37:55 -0300

    Configuración de Socket

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import express from "express";
-
 import usersRouter from "./routes/users.routes.js"
 import routerProd from "./routes/products.routes.js";
 import routerCart from "./routes/carts.routes.js";
 import { engine } from "express-handlebars";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { createServer } from "http"
+import { Server } from "socket.io";
 
-const PORT = 8080
-const app = express()
-//Motor de Plantillas 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const PORT = 8080
+const app = express()
+const server = createServer(app)
 
+// Carpeta estatic
+app.use(express.static(__dirname + './public'))
+//Motor de Plantillas 
 app.engine('handlebars', engine()); //inicializa
 app.set('view engine', 'handlebars'); //indica que dentro de la carpeta tendremos archivos handlebars
 app.set('views', (__dirname, './views'));  //ubicación de la carpeta 
@@ -20,8 +24,13 @@ app.set('views', (__dirname, './views'));  //ubicación de la carpeta
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 /// Routes 
-
 app.use('/api/products', routerProd)
 app.use('/api/carts', routerCart)
 app.use('/users', usersRouter)
-app.listen(PORT, () => { console.log(`Server run in port ${PORT}`) })  
+// Socket
+const io = new Server(server)
+io.on('connection', (socket) => {
+    console.log('User conectado')
+})
+
+server.listen(PORT, () => { console.log(`Server run in port ${PORT}`) })  
